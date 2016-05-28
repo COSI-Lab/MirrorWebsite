6c50e50628733cfd8b589ef3dae367752a9cf188
Benjamin Lannon <brlannon118@gmail.com>
2016-05-28 14:17:35 -0400

    Moving majority of header tags to external file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
   return gulp.src("./scss/main.scss")
+		.pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}))
+		.pipe(sourcemaps.write())
     .pipe(gulp.dest("./css"));
 });
 

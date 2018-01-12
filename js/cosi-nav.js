f6c972992626365173f111f373f4c63ebc6a2aa5
Benjamin Lannon <lannonbr@clarkson.edu>
2018-01-12 12:04:56 -0500

    Dynamically loading cosi-nav

--- js/cosi-nav.js
+++ js/cosi-nav.js
@@ -1,11 +1,21 @@
-fetch('https://dubsdot.cslabs.clarkson.edu/cosi-nav.json')
+window.onload = function() {
+  fetch("https://dubsdot.cslabs.clarkson.edu/cosi-nav.json")
     .then(res => res.json())
     .then(json => {
-        let links = json.links;
-        let linkList = document.querySelector('cosi-nav');
-        links.forEach(link => {
-            let newLink = document.createElement('li');
+      fetch("https://dubsdot.cosi.clarkson.edu/cosi-nav.css")
+        .then(res => res.text())
+        .then(css => {
+          let style = document.createElement("style");
+          style.innerText = css;
+          document.head.appendChild(style);
+
+          let links = json.links;
+          let linkList = document.querySelector("cosi-nav");
+          links.forEach(link => {
+            let newLink = document.createElement("li");
             newLink.innerHTML = `<a href="${link.url}">${link.name}</a>`;
             linkList.appendChild(newLink);
+          });
         });
     });
+};

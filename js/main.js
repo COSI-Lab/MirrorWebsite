0ef33552d7aa90307977b6687cfa65c14e493c29
Benjamin Lannon <lannonbr@clarkson.edu>
2018-04-07 16:18:00 -0400

    Updating menu-button to toggle icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-	$(".menu-button").click(function () {
-		$("#navmobile").slideToggle(300);
-	});
+$(document).ready(function() {
+  $("#menu-button").click(function() {
+    $("#navmobile").slideToggle(300);
+    const menuButton = document.getElementById("menu-button");
+    if (menuButton.className == "fa fa-bars") {
+      menuButton.className = "fa fa-times";
+    } else {
+      menuButton.className = "fa fa-bars";
+    }
+  });
 });
